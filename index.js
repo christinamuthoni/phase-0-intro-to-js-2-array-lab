5eecf78670ecf45203e78c88efa50146ba678b29
christine <christine.muriungi@student.moringaschool.com>
2023-10-17 15:57:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    let name1 = [...cats, "Broom"]
+    return name1;
+}
+function prependCat(name){
+const allCats = ["Arnold", ...cats]
+return allCats;
+}
+function removeLastCat(){
+    const copyCats = cats.slice(0, cats.length -1);
+    return copyCats;
+}
+function removeFirstCat(){
+    const catts= cats.slice(1)
+    return catts
+}
